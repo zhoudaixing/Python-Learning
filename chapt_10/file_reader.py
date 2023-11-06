9d1a7f3785ca3f1a8249301df28f31d6d9ba8eef
zhoudaixing-wsl <zhoudaixing@gmail.com>
2023-11-06 14:04:31 +0800

    change structure of menu

--- chapt_10/file_reader.py
+++
@@ -1,6 +0,0 @@
-file_path = 'D:\\Development\\py\\chapt_10\\pi_digits.txt'
-with open(file_path) as file_object:
-    """contents = file_object.read()
-    print(contents)"""
-    for test in file_object:
-        print(test)
--- chapt_10/write_message.py
+++
@@ -1,4 +0,0 @@
-filename = 'programming.txt'
-
-with open(filename, 'a') as file_object:
-    file_object.write("I love coding")
